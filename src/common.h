c38bf5f4467b54ff4cf5f0c7017c69caa50a5a31
ColeChan <chenwenjie27@gmail.com>
2016-04-01 17:07:17 +0800

    Add status

--- src/common.h
+++ src/common.h
@@ -4,4 +4,8 @@
 const unsigned INI_MATRIX_COL = 32;
 const unsigned INI_MATRIX_ROW = 32;
 
+const short STAT_SUCCESS        = 0;
+const short STAT_ERROR          = 1;
+const short STAT_ADD_ELE_ERR    = 10;
+
 #endif //MATRIX_COMMON_H_
