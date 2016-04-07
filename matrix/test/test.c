ecd60f6e625c08fc77018402b55b5a954362c3b4
ColeChan <chenwenjie27@gmail.com>
2016-04-07 15:53:21 +0800

    Update bash_gen

--- matrix/test/test.c
+++ matrix/test/test.c
@@ -20,7 +20,7 @@ int main() {
                         &test_new_element,
                         &test_matrix_add_element};
     unsigned len = sizeof(functions)/sizeof(functions[0]);
-    
+
     for(int i = 0; i < len; i++) {
         int res = (*functions[i])();
         if(res != 0)
