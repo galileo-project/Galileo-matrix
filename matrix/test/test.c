8dc8929cb680dacaa07c8ef4b23b1463d8e5baf1
ColeChan <chenwenjie27@gmail.com>
2016-04-07 14:10:00 +0800

    add unit test mod

--- matrix/test/test.c
+++ matrix/test/test.c
@@ -1,3 +1,57 @@
+#include "matrix.h"
+#include "element.h"
+
+typedef void (*Func)(void);
+
+int test_new_matrix(void);
+int test_new_element(void);
+int test_matrix_add_element(void);
+
 int main() {
+    Func functions[] = {test_new_matrix, 
+                        test_new_element,
+                        test_matrix_add_element}
+    unsigned len = sizeof(functions)/sizeof(Func)
+    
+    for(int i = 0; i < len; i++) {
+        int res = functions[i]()
+        if(res != 0)
+            return res;
+        else
+            continue;
+    }
+                                 
     return 0;
+}
+
+int test_new_matrix(void) {
+    Matrix *matrix = matrix_new();
+    if(matrix == NULL)
+        return 1;
+    else
+        return 0
+}
+
+int test_new_element(void) {
+    Matrix *matrix = matrix_new();
+    if(matrix == NULL)
+        return 1;
+    else
+        return 0
+}
+
+int test_matrix_add_element(void) {
+    Matrix *matrix = matrix_new();
+    if(matrix == NULL)
+        return 1;
+        
+    Element *element = element_new(0,0,100);
+    if(element == NULL)
+        return 1;
+    
+    Status status matrix_add(element);
+    if(status == STAT_SUCCESS)
+        return 0;
+    else
+        return 1;
 }
