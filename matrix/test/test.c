4d07bba05bb8badcf9b4145fa3c355c325bceeb7
ColeChan <chenwenjie27@gmail.com>
2016-04-07 16:07:19 +0800

    Add test log

--- matrix/test/test.c
+++ matrix/test/test.c
@@ -3,6 +3,13 @@
 #include "matrix.h"
 #include "element.h"
 
+#define LOG_ERR(msg) do{\
+        printf("ERROR: %s .\n", msg);\
+    }while(0)
+#define LOG_SUCCESS(msg) do{\
+        printf("SUCCESS: %s .\n", msg);\
+    }while(0)
+
 const unsigned TEST_RAW_LEN = 100;
 const unsigned TEST_COL_LEN = 100;
 
@@ -28,7 +35,8 @@ int main() {
         else
             continue;
     }
-                                 
+    
+    LOG_SUCCESS("all mod test sucess.");
     return 0;
 }
 
@@ -36,41 +44,53 @@ int main() {
 
 int test_config_new(void) {
     Config *config = config_new(TEST_RAW_LEN, TEST_COL_LEN);
-    if(config == NULL)
+    if(config == NULL) {
+        LOG_ERR("new config");
         return 1;
-    else
+    } else {
+        LOG_SUCCESS("new config");
         return 0;
+    }
 }
 
 int test_new_matrix(void) {
     Config *config = config_new(TEST_RAW_LEN, TEST_COL_LEN);
     Matrix *matrix = matrix_new(config);
-    if(matrix == NULL)
+    if(matrix == NULL) {
+        LOG_ERR("new matrix");
         return 1;
+    }
     
     safe_free(config);
     safe_free(matrix);
+    LOG_SUCCESS("new matrix");
     return 0;
 }
 
 int test_new_element(void) {
     Element *element = element_new(0,0,100);
-    if(element == NULL)
+    if(element == NULL) {
+        LOG_ERR("new element");
         return 1;
+    }
 
     safe_free(element);
+    LOG_SUCCESS("new element");
     return 0;
         
 }
 
 int test_matrix_add_element(void) {
     Matrix *matrix = new_matrix();
-    if(matrix == NULL)
+    if(matrix == NULL) {
+        LOG_ERR("add element to matrix -> new matrix");
         return 1;
+    }
         
     Element *element = element_new(0,0,100);
     if(element == NULL) {
         safe_free(matrix);
+        LOG_ERR("add element to matrix -> new element");
         return 1;
     }
     
@@ -78,11 +98,13 @@ int test_matrix_add_element(void) {
     if(status != STAT_SUCCESS) {
         safe_free(matrix);
         safe_free(element);
+        LOG_ERR("add element to matrix");
         return 1;    
     }
     
     safe_free(element);
     safe_free(matrix);
+    LOG_SUCCESS("add element to matrix");
     return 0;
 }
 
