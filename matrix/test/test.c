39c84df89b305d81ec73e065d7cb02499c61b4da
ColeChan <chenwenjie27@gmail.com>
2016-04-07 18:25:36 +0800

    Update unit test mod

--- matrix/test/test.c
+++ matrix/test/test.c
@@ -16,28 +16,37 @@ const unsigned TEST_COL_LEN = 100;
 typedef int (*Func)(void);
 
 static Matrix *new_matrix(void);
+static int fill_matrix(Matrix*, unsigned, unsigned);
 int test_new_matrix(void);
 int test_new_element(void);
 int test_matrix_add_element(void);
 int test_config_new(void);
+int test_matrix_find_element(void);
+int test_matrix_clear(void);
+int test_matrix_update(void);
 
 int main() {
     Func functions[] = {&test_config_new,
                         &test_new_matrix, 
                         &test_new_element,
-                        &test_matrix_add_element};
+                        &test_matrix_add_element,
+                        &test_matrix_find_element,
+                        &test_matrix_clear,
+                        &test_matrix_update};
     unsigned len = sizeof(functions)/sizeof(functions[0]);
+    int result = 0;
 
     for(int i = 0; i < len; i++) {
-        int res = (*functions[i])();
-        if(res != 0)
-            return res;
-        else
-            continue;
+        if((*functions[i])() != 0)
+            result = 1;
     }
     
-    LOG_SUCCESS("success");
-    return 0;
+    if(result == 0)
+        LOG_SUCCESS("success");
+    else
+        LOG_ERR("failed");
+       
+    return result;
 }
 
 //////// test functions //////////
@@ -108,9 +117,102 @@ int test_matrix_add_element(void) {
     return 0;
 }
 
+int test_matrix_find_element(void) {
+    Matrix *matrix = new_matrix();
+    int res = fill_matrix(matrix, 20, 20);
+    if(res != 0) {
+        LOG_ERR("test matrix find element -> fill matrix error");
+        return res;
+    }
+    
+    Eelement *element = matrix_find_by_pos(matrix, 3, 15);
+    if(element->value == 3*15) {
+        LOG_SUCCESS("fine element from matrix by pos");
+    } else {
+        LOG_ERR("find element from matrix by pos");
+        return 1;
+    }
+    
+    Eelement *element = matrix_find_by_val(matrix, 3*15);
+    if(element->raw == 3 && element->col == 15) {
+        LOG_SUCCESS("fine element from matrix by value");
+    } else {
+        LOG_ERR("find element from matrix by value");
+        return 1;
+    }
+    
+    return 0;
+}
+
+int test_matrix_clear(void) {
+    Matrix *matrix = new_matrix();
+    int res = fill_matrix(matrix, 20, 20);
+    if(res != 0) {
+        LOG_ERR("test matrix clear -> fill matrix error");
+        return res;
+    }
+    
+    Status status = matrix_clear(matrix);
+    if(status != STAT_SUCCESS) {
+        LOG_ERR("test matrix clear");
+        return 1;
+    } else {
+        LOG_SUCCESS("test matrix clear");
+        return 0;
+    }
+    
+}
+
+int test_matrix_update(void) {
+    Matrix *matrix = new_matrix();
+    int res = fill_matrix(matrix, 20, 20);
+    if(res != 0) {
+        LOG_ERR("test matrix update -> fill matrix error");
+        return res;
+    }
+    
+    Element *new_element = element_new(10, 10, 10+10);
+    Status statux = matrix_update(matrix, new_element);
+    if(status != STAT_SUCCESS) {
+        LOG_ERR("test matrix update");
+        return 1;
+    } else {
+        Element *element = matrix_find_by_pos(matrix, 10, 10);
+        if(element == NULL) {
+            LOG_ERR("test matrix update -> find element error");
+            return 1;
+        } else {
+            if(new_element->value == element->value) {
+                LOG_SUCCESS("test matrix update");
+                return 0;
+            } else {
+                LOG_ERR("test matrix update, can't update")
+                return 1;
+            }
+        }
+    }
+        
+}
+
 //////// test functions end //////////
 
 static Matrix *new_matrix(void) {
     Config *config = config_new(TEST_RAW_LEN, TEST_COL_LEN);
     return matrix_new(config);
+}
+
+static int fill_matrix(Matrix *matrix, unsigned raw, unsigned col) {
+    for(unsigned i =0; i < raw; i++) {
+        for(unsigned j = 0; j < col; j++) {
+            Element *element = element_new(i, j, i*j);
+            if(element == NULL)
+                return 1;
+                
+            Status status = matrix_add(matrix, element);
+            if(status != STAT_SUCCESS)
+                return 1;
+        }
+    }
+    
+    return 0;
 }
